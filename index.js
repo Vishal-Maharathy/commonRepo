18980bd49952b9b86205c3a297e5693a467cf34c
Vishal Maharathy <vishalmaharathy76@gmail.com>
2024-02-23 05:35:11 +0530

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+// Initialize an empty Map
+var usersMap = new Map();
+// Add users to the map using .set
+usersMap.set('abc123', { id: 'abc123', name: 'John Doe' });
+usersMap.set('xyz789', { id: 'xyz789', name: 'Jane Doe' });
+// Accessing a value using .get
+// console.log(usersMap.get('abc123')); // Output: { id: 'abc123', name: 'John Doe' }
+console.log("HELLO");
